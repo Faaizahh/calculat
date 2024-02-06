a4aea658d192f85bd18ed707f52a1567487b0dfc
Fawaz <abdulsalamfawaz4@gmail.com>
2024-02-06 20:25:00 +0100

    changes

--- script.js
+++ script.js
@@ -5,16 +5,19 @@
     let equal = document.querySelector('.btn-equal');
 
     buttons.forEach(function (button) {
-        button.addEventListener('click', function (e) {
-            let value = e.target.dataset.num;
-            screen.value += value;
-        })
+        if (!button.classList.contains('btn-equal')) {
+            button.addEventListener('click', function (e) {
+                let value = e.target.dataset.num;
+                screen.value += value;
+            })
+        }
     });
 
     equal.addEventListener('click', function (e) {
         if (screen.value === '') {
             screen.value = "Please enter";
         } else {
+            console.log(screen.value)
             let answer = eval(screen.value);
             screen.value = answer;
         }
